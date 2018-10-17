91b0e2359b5167106e4850716b3d75ea5bb0e1dd
Wendy Billings <wbillings@studentbody.byu.edu>
2018-10-17 16:25:14 -0600

    edit so properly updates, some design

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 var app = angular.module("myApp", [])
 app.controller("mainCtrl", mainCtrl)
-app.directive("movie", movieDirective);
 
 function mainCtrl($scope, $http){
     $scope.movies = [];
-    $scope.movielist = [];
+    
 
     $scope.Update = function(form){
-        
+        $scope.movielist = [];
+
         var url = "https://api.themoviedb.org/3/discover/movie?api_key=ba7b6743e0dbfac3faa0607ce7732674&language=en-US&sort_by=popularity.desc&page=1";
         
         var startDate = form.fromyear + "-01-01";
@@ -18,28 +18,18 @@ function mainCtrl($scope, $http){
 
         var completeUrl = url + dateQuery + genreQuery;
         $http.get(completeUrl).then(function(response){
+            console.log(response);
             for(var i = 0; i < response.data.results.length; i++){
                 $scope.movielist.push({
                     title: response.data.results[i].title,
                     overview: response.data.results[i].overview,
-                    image: "https://image.tmdb.org/t/p/w500" + response.data.results[i].poster_path
+                    image: "https://image.tmdb.org/t/p/w500" + response.data.results[i].poster_path,
+                    date: (response.data.results[i].release_date).substring(0,4)
                     
                 });
-                console.log("https://image.tmdb.org/t/p/w500" + response.data.results[i].poster_path)
             }
-            
-            console.log(response.data.results)
-            console.log($scope.movielist);
         });
 
     }
-
-    /*$scope.searchMovies = function(stuff){
-        var url = "https://api.themoviedb.org/3/discover/movie?api_key=ba7b6743e0dbfac3faa0607ce7732674&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&primary_release_date.gte=2018-01-01&primary_release_date.lte=2018-12-31&vote_average.gte=6&with_genres=28";
-        $http.get(url).then(function(response){
-            $scope.movies = response.data;
-        });
-    }
-    */
 }
 
